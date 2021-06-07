17cf5889760d7f4dca26a1f9cbb686a18b873e57
EReich <smartbutt10@gmail.com>
2021-06-07 18:04:15 -0400

    fixed sql

--- DataBase.sql
+++ DataBase.sql
@@ -1,12 +1,13 @@
+
+
 CREATE TABLE OrgMembers (
     email VARCHAR(255) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     inGameName VARCHAR(255) NOT NULL UNIQUE,
     discordId VARCHAR(255) UNIQUE,
     dbRole VARCHAR(255) NOT NULL,
-    CHECK(dbRole = 'Player' OR dbRole = 'Staff'),
-    ON UPDATE CASCADE,
-    ON DELETE CASCADE
+    CONSTRAINT playerorstaff CHECK (dbRole = 'Player' OR dbRole = 'Staff')
+    
 );
 
 CREATE TABLE Players (
@@ -15,31 +16,28 @@ CREATE TABLE Players (
     gradDate DATE NOT NULL,
     teamID VARCHAR(255) NOT NULL,
     teamRole VARCHAR(255),
-    CHECK(
+    CONSTRAINT classCodes CHECK(
         classStanding = 'FR' OR 
         classStanding = 'SO' OR 
         classStanding = 'JR' OR 
         classStanding = 'SR'
     ),
-    FOREIGN KEY (inGameName) REFERENCES OrgMembers(inGameName),
-    ON UPDATE CASCADE,
-    ON DELETE CASCADE
+    FOREIGN KEY (inGameName) REFERENCES OrgMembers(inGameName) ON DELETE CASCADE
 );
 
 CREATE TABLE Staff(
     inGameName VARCHAR(255) NOT NULL PRIMARY KEY,
     role VARCHAR(255),
     gameTitle VARCHAR(255),
-    FOREIGN KEY (inGameName) REFERENCES OrgMembers(inGameName),
+    FOREIGN KEY (inGameName) REFERENCES OrgMembers(inGameName) ON DELETE CASCADE,
     FOREIGN KEY (gameTitle) REFERENCES Games(Title)
-    ON UPDATE CASCADE,
     ON DELETE CASCADE
 );
 
 CREATE TABLE Games(
     title VARCHAR(255) NOT NULL PRIMARY KEY,
     coach VARCHAR(255) NOT NULL,
-    FOREIGN KEY (coach) REFERENCES OrgMembers(name),
+    FOREIGN KEY (coach) REFERENCES OrgMembers(name)
     ON DELETE CASCADE
 );
 
@@ -48,7 +46,7 @@ CREATE TABLE Teams(
     numPlayers INT,
     teamName VARCHAR(255) NOT NULL,
     game VARCHAR(255),
-    FOREIGN KEY (game) REFERENCES Games(title)
+    FOREIGN KEY (game) REFERENCES Games(title) ON DELETE CASCADE
 );
 
 CREATE TABLE Leagues(
@@ -58,8 +56,8 @@ CREATE TABLE Leagues(
     endDate DATE,
     teamID VARCHAR(3) NOT NULL,
     game VARCHAR(255) NOT NULL,
-    FOREIGN KEY (game) REFERENCES Games(title),
-    FOREIGN KEY (teamID) REFERENCES Teams(teamID),
+    FOREIGN KEY (game) REFERENCES Games(title) ON DELETE CASCADE,
+    FOREIGN KEY (teamID) REFERENCES Teams(teamID)
     ON DELETE CASCADE
 );
 
@@ -67,7 +65,7 @@ CREATE TABLE Matches(
     matchID VARCHAR(255) NOT NULL PRIMARY KEY,
     league VARCHAR(255) NOT NULL,
     awayTeam VARCHAR(255) NOT NULL,
-    FOREIGN KEY (league) REFERENCES Leagues(leagueID)
+    FOREIGN KEY (league) REFERENCES Leagues(leagueID) 
 );
 
 CREATE TABLE Stats(
