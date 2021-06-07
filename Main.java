cba7df3716eb688c09bfc6dcb1e4ed367fd591f2
Kyle Heestand <kheestand@heestand.tech>
2021-06-07 17:18:49 -0400

    GUI

--- Main.java
+++ Main.java
@@ -1,7 +1,23 @@
-public class Main {
+import javax.swing.SwingUtilities;
 
-    public static void main(String[] args) {
-        System.out.println("hello world");
+public class Main {
+    
+    Main(String[] args) {
+        // Invoked on the event dispatching thread.
+        // Do any initialization here.
     }
 
+    public void show() {
+        // Show the UI.
+    }
+
+    public static void main(final String[] args) {
+        // Schedule a job for the event-dispatching thread:
+        // creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Main(args).show();
+            }
+        });
+    }
 }
