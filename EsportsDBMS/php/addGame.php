718f8f0c317c110de71ffbcdc9e34ece1173b925
EReich <smartbutt10@gmail.com>
2021-06-08 15:37:04 -0400

    Fixed formatting, implemented addTeam

--- EsportsDBMS/php/addGame.php
+++ EsportsDBMS/php/addGame.php
@@ -1,6 +1,12 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <TITLE>Webtech Test Page</TITLE>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <style type="text/css">
@@ -64,5 +70,10 @@ a:active {
             <td></td>
     </tr>
 </div>
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
--- EsportsDBMS/php/addTeam.php
+++ EsportsDBMS/php/addTeam.php
@@ -7,74 +7,79 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-<TITLE>Kettering Esports DB Homepage</TITLE>
+<TITLE>Webtech Test Page</TITLE>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <style type="text/css">
 a:link {
-	color: #006633;
+        color: #006633;
 }
 a:visited {
-	color: #003366;
+        color: #003366;
 }
 a:hover {
-	color: #009933;
+        color: #009933;
 }
 a:active {
-	color: #990000;
-}
-
-a.button {
-    -webkit-appearance: button;
-    -moz-appearance: button;
-    appearance: button;
-
-    text-decoration: none;
-    color: initial;
+        color: #990000;
 }
 
 .content {
   max-width: 500px;
   margin: auto;
 }
-
 </style>
 </head>
 <body>
+<div class="content">
+	<?php
+		
 
-    <div div class="content">
-        <form action = "php/addTeam.php" method="post">
-            <div class="addTeamForm">
-                <label for="teamID">Team ID</label>
-                <input type="text" class="form-control" id="teamID" name="teamID" placeholder="Enter Team ID, unique 3 character combo">
-            </div>
-            <div class="addTeamForm">
-                <label for="numPlayers">Number of Players</label>
-                <input type="text" class="form-control" id="numPlayers" name="numPlayers" placeholder="Enter Number of Players">
-            </div>
-            <div class="addTeamForm">
-                <label for="teamName">Team Name</label>
-                <input type="text" class="form-control" id="teamName" name="teamName" placeholder="Enter Team Name">
-            </div>
-            <div class="addTeamForm">
-                <label for="game">Game Title</label>
-                <input type="text" class="form-control" id="game" name="game" placeholder="Enter Game Title">                
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-		<tr>
+		$servername = "localhost";
+		$username = "reic6330";
+		$password = "702036330";
+		$dbname = "test";
+
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		
+		if ($conn -> connect_error){
+			die("Connection failed: " . $conn->connect_error);
+		}
+		
+		$teamID = $_POST['teamID'];
+		$numPlayers = $_POST['numPlayers'];
+		$teamName = $_POST['teamName'];
+		$game = $_POST['game'];
+		
+		
+		
+		$sql = " INSERT INTO `Teams`
+			VALUES('$teamID', '$numPlayers', '$teamName', '$game');";
+		
+		
+		if ($conn->query($sql) === TRUE) {
+			echo "New records created successfully";
+		} else {
+			echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+		
+		$conn->close();
+	
+	?>
+	<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<tr>
             <td></td>
             <td><a href="http://webtech.kettering.edu/~reic6330/ESportsDBMSHome.html" id = "homebutton" class="btn btn btn-primary">Home</a></td>
             <td></td>
     </tr>
-    </div>
-
+</div>
 <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-
 </body>
 </html>
