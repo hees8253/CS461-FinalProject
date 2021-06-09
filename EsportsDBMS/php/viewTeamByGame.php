d313b3b703bab16f9a40551d8c5078b08d61b3b7
EReich <smartbutt10@gmail.com>
2021-06-09 13:41:36 -0400

    Update viewTeamByGame.php

--- EsportsDBMS/php/viewTeamByGame.php
+++ EsportsDBMS/php/viewTeamByGame.php
@@ -33,6 +33,9 @@ a:active {
 <div class="content">
 	<?php
 		
+		ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
 
 		$servername = "localhost";
 		$username = "reic6330";
