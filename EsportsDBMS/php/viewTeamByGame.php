43a9c26a0346edaad6a89a71fa951f3c0301c88a
EReich <smartbutt10@gmail.com>
2021-06-09 08:02:24 -0400

    worked on viewTeamByGame, still doesn't work for some reason
    AAAAAAAAAAAAAAAAAAA

--- EsportsDBMS/php/viewTeamByGame.php
+++ EsportsDBMS/php/viewTeamByGame.php
@@ -7,64 +7,76 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-<TITLE>Kettering Esports DB Homepage</TITLE>
+<TITLE>Kettering Esports Staff Directory</TITLE>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <style type="text/css">
 a:link {
-	color: #006633;
+        color: #006633;
 }
 a:visited {
-	color: #003366;
+        color: #003366;
 }
 a:hover {
-	color: #009933;
+        color: #009933;
 }
 a:active {
-	color: #990000;
-}
-
-a.button {
-    -webkit-appearance: button;
-    -moz-appearance: button;
-    appearance: button;
-
-    text-decoration: none;
-    color: initial;
+        color: #990000;
 }
 
 .content {
-  max-width: 500px;
+  max-width: 1080px;
   margin: auto;
 }
-
 </style>
 </head>
 <body>
 <div class="content">
+	<?php
+		
 
+		$servername = "localhost";
+		$username = "reic6330";
+		$password = "702036330";
+		$dbname = "test";
+
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		
+		if ($conn -> connect_error){
+			die("Connection failed: " . $conn->connect_error);
+		}
+		
+		$gameTile = $_POST['gameTitle'];
+		
+		$sql = "SELECT t.teamName, t.numPlayers, t.game 
+				FROM Teams AS t WHERE t.game = '$gameTitle'
+				";
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+			echo "<br>| Team: | Game: | Max Players: <br> "
+			while($row = $result->fetch_assoc()){
+				echo .$row["teamName"].
+				" | ".$row["game"].
+				" | ". $row["numPlayers"]. 
+				
+				"<br>";           
+			}
+		} else {
+			echo "0 Results";
+		}
+	
+	?>
+	<br>
 	<tr>
-        <form action="php/viewTeamByGame.php" method="post">
-            <div class="searchTeamsByGame">
-                <label for="gameTitle">Enter Game Title</label>
-                <input type="text" class="form-control" id="gameTitle" name="gameTitle" placeholder="Enter exact game title, e.g 'Rainbow 6: Siege'">
-            </div>
-			<button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-          
-    </tr>
-    
-	<tr>
-          <td></td>
-          <td><a href="http://webtech.kettering.edu/~reic6330/ESportsDBMSHome.html" id = "homebutton" class="btn btn btn-primary">Home</a></td>
-          <td></td>
+            <td></td>
+            <td><a href="http://webtech.kettering.edu/~reic6330/ESportsDBMSHome.html" id = "homebutton" class="btn btn btn-primary">Home</a></td>
+            <td></td>
     </tr>
 <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
 </div>
 </body>
 </html>
